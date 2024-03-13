ad032152708006c1bcc7b4ad658797f6e6a627cd
PedroPertusi <pedrovazpertusi@gmail.com>
2024-03-13 11:07:12 -0300

    fix

--- src/main/java/organice/auth/AuthApplication.java
+++ src/main/java/organice/auth/AuthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = {
-    "insper.store.account"
+    "organice.account"
 })
 @EnableDiscoveryClient
 @SpringBootApplication
